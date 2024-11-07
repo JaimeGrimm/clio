755bb74541ae26c930746af614f45069cb27970f
Jaime Grimm <jaimegrimm@Jaimes-MacBook-Air-5.local>
2024-11-07 13:18:27 -0500

    Added sequential information for exploring autocorrelation

--- scripts/data_preparation.R
+++ scripts/data_preparation.R
@@ -22,4 +22,21 @@ data<-data %>%
                         TRUE~'control')) %>%
   filter(data$Task != "STANDARD") #remove standards
 NAs <- which(is.na(data$Quantity))
-data$Quantity[NAs] <- 0
+data$Quantity[NAs] <- 0 
+
+#Add sequential order of samples:
+regexp <- "[[:digit:]]+"
+data <- data %>%
+  mutate(seq  = substr(str_extract(Sample.Name, regexp), 1, 2))%>%
+  filter(Sample.Name != "EB0108" & Sample.Name != "EB0108.1" 
+         & Sample.Name != "CT0804" & Sample.Name != "qPCRBlank" &
+         Sample.Name != "qPCRblank" & Sample.Name != "Messedup"
+         & Sample.Name != "water") #%>%
+  #dplyr::select(Sample.Name, seq)%>%
+  #arrange(seq)
+
+#Manually assign seq values for controls  
+data$seq[data$Sample.Name=="CT0884"] <- 0
+data$seq[data$Sample.Name=="CT0837"] <- 8.25
+data$seq[data$Sample.Name=="CT0931"] <- 8.5
+data$seq[data$Sample.Name=="CT0917"] <- 25
