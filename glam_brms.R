a8ff8c35926a2fa9b2e4f94c03ed7bf07e000936
Jaime Grimm <jaimegrimm@esc3010-grimm-krkosek.eeb.utoronto.ca>
2024-11-18 12:59:12 -0500

    Created script

---
+++ glam_brms.R
@@ -0,0 +1,4 @@
+####Analysis of Clio channel and knight inlet data collected summer 2024#
+#Bayesian regression models using stan. The goal is to model species distributions with species ID as a random effect
+#Created November 18, 2024
+#Last updated November 18, 2024
