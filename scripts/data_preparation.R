cffd3e3e66638d12504fed6ca62f0685c04cb3c4
Jaime Grimm <137961629+JaimeGrimm@users.noreply.github.com>
2025-07-22 11:16:07 -0400

    Added new data file to preparation

--- scripts/data_preparation.R
+++ scripts/data_preparation.R
@@ -1,13 +1,17 @@
 #Load and prepare data ----
+library(tidyverse)
 # using readr's read_csv for better typing
-data1 <- read_csv("~/Documents/GitHub/clio/data/clio.csv", skip = 42, show_col_types = FALSE)
-data2 <- read_csv("~/Documents/GitHub/clio/data/clio2.csv", skip = 42, show_col_types = FALSE)
-data3 <- read_csv("~/Documents/GitHub/clio/data/clio3.csv", skip = 42, show_col_types = FALSE)
+data1 <- read_csv("./data/clio.csv", skip = 42, show_col_types = FALSE) %>% 
+  filter(`Sample Name` != "KN1922", `Sample Name` != "CL2037") #These two filters were outliers. Reran in data4 to confirm. Will use data4 amplifications
+data2 <- read_csv("./data/clio2.csv", skip = 42, show_col_types = FALSE)
+data3 <- read_csv("./data/clio3.csv", skip = 42, show_col_types = FALSE)
+data4 <- read_csv("./data/clio4.csv", skip = 42, show_col_types = FALSE) 
+
 envdata <- read_csv("~/Documents/GitHub/clio/data/rawsamplingdata.csv")
 
-data <- bind_rows(data1, data2, data3) %>%
+data <- bind_rows(data1, data2, data3, data4) %>%
   # simplify column names:
-  rename(Target = `Target Name`, Sample = `Sample Name`, Amp = `Amp Status`) %>%
+  rename(Target = `Target Name`, Sample = `Sample Name`, Amp = `Amp Status`, well = 'Well Position') %>%
   # add `Site` column:
   mutate(Site = case_when(
     str_detect(Sample, "CL")~"Clio",
@@ -20,17 +24,18 @@ data <- bind_rows(data1, data2, data3) %>%
     Target != "ctv-2",
     Sample != "EB0108",
     Sample != "EB0108.1",
-    Sample != "CT0804",
     Sample != "qPCRBlank",
     Sample != "qPCRblank",
+    Sample != "qPRCBlank",
     Sample != "Messedup",
-    Sample != "water") %>%
+    Sample != "water",
+    mAMP != "ATYPICAL") %>%
   # grab sequence numbers:
   mutate(Sequence = as.numeric(substr(str_extract(Sample, "[[:digit:]]+"), 1, 2))) %>%
   # controls were run at the beginning & end of each day; increment second day's sequence number by 2:
   mutate(Sequence = if_else(Sequence < 9, Sequence, Sequence + 2)) %>%
   # grab only the columns we need:
-  dplyr::select(Sample, Sequence, Site, Target, Quantity, Amp) %>% 
+  dplyr::select(Sample, Sequence, Site, Target, Quantity, Amp, well, CT, mAMP) %>% 
   mutate(fullname = case_when(Target == "Te_mar" ~ "T. maritimum", 
                               Target == "Te_fin" ~ "T. finnmarkense", 
                               Target == "sch" ~ "Cand. S. salmonis",
@@ -38,6 +43,7 @@ data <- bind_rows(data1, data2, data3) %>%
                               Target == "pisck_sal" ~ "P. salmonis", 
                               Target == "pa_ther" ~ "P. theridion",
                               Target == "env" ~ "ENV"))
+data$Sample[data$Sample =="CT0804"] <- "CT0884"
 
 # Manually assign sequence numbers for controls:
 data$Sequence[data$Sample=="CT0884"] <- 0 #beginning of day 1
@@ -45,6 +51,7 @@ data$Sequence[data$Sample=="CT0837"] <- 9 #end of day 1
 data$Sequence[data$Sample=="CT0931"] <- 10 #beginning of day 2
 data$Sequence[data$Sample=="CT0917"] <- 27 #end of day 2
 # -
+data <- data %>% filter(!grepl("CT", Sample))
 
 # ### adding a `PrevConc` covariate
 #
@@ -58,7 +65,7 @@ data$Sequence[data$Sample=="CT0917"] <- 27 #end of day 2
 # failed amplifications that still have a non-zero quantity associated with them
 # +
 data$Conc <- if_else(is.na(data$Quantity), 0, data$Quantity)
-data <- data %>% filter(!(Amp == "No Amp" & Conc > 0)) 
+foo <- data %>% filter((mAMP == "NOAMP" & Conc > 0)) 
 
 # check out avg. DNA concentration against sequence number
 data %>% group_by(Target, Sequence) %>%
@@ -91,7 +98,10 @@ envdata_nc <- envdata %>%
   mutate(Conc = Conc / 100, PrevConc = PrevConc / 100) # scale for easier sampling
 
 #Look at contamination in controls ----
-controls <- bind_rows(data1, data2, data3) %>% 
+library(PNWColors)
+pal <- pnw_palette("Starfish")
+
+controls <- bind_rows(data1, data2, data3, data4) %>% 
   rename(Target = `Target Name`, Sample = `Sample Name`, Amp = `Amp Status`) %>%
   mutate(Site = case_when(
     str_detect(Sample, "CL")~"Clio",
@@ -103,21 +113,30 @@ controls <- bind_rows(data1, data2, data3) %>%
                               Target == "sasa" ~ "Atlantic salmon", 
                               Target == "pisck_sal" ~ "P. salmonis", 
                               Target == "pa_ther" ~ "P. theridion",
-                              Target == "env" ~ "ENV"))
+                              Target == "env" ~ "ENV")) %>% 
+  filter(Target != "ascv", Target != "ctv-2", mAMP != "ATYPICAL")
 controls$Conc <- if_else(is.na(controls$Quantity), 0, controls$Quantity)
+controls$Sample[controls$Sample=="qPCRblank"] <- "qPCRBlank"
+controls$Sample[controls$Sample=="qPRCBlank"] <- "qPCRBlank"
+controls$Sample[controls$Sample=="water"] <- "qPCRBlank"
+controls$Sample[controls$Sample =="CT0804"] <- "CT0884"
     
-cont.only<- controls %>% filter(Site != "Clio", Site != "Knight", Conc > 0, Amp == "Amp",
-                               Task != "STANDARD") 
+cont.only<- controls %>% filter(Site != "Clio", Site != "Knight", #Amp == "Amp",
+                               Task != "STANDARD", Sample != "Messedup") 
 temp <- data %>% filter(fullname == "Atlantic salmon" | fullname == "Cand. S. salmonis" | fullname == "P. salmonis" | 
                         fullname == "P. theridion")
 ggplot()+
-  geom_violin(data = temp, aes(y = Conc, x = fullname)) +
-  geom_jitter(data = cont.only, aes(y=Conc, x = fullname, color = Sample), width = 0.1, height = 0, size = 2) +
-  ylab("log(1 + eDNA contamination (copies/μL))") +
+  geom_jitter(data = data, aes(y = Conc, x = fullname), color = "grey70", alpha=0.3, width = 0.3, size = 2.5) +
+  geom_point(data = cont.only, aes(y=Conc, x = fullname, color = Sample), position = position_jitter(seed = 2, width = 0.2, height = 0), size = 3.5) +
+  ylab("eDNA concentration (copies/μL)") +
   xlab("Target") +
-  scale_y_continuous(trans="log1p") +
+  scale_y_continuous(trans="sqrt") +
   theme_bw(base_size = 18)+
-  theme(axis.text.x = element_text(angle = 45, vjust =0.6)) +
-  scale_color_discrete(labels = c("CT0884" = "Start of day 1", "CT0837" = "End of day 1", "CT0917" = "End of day 2", 
-                                  "EB0108" = "Extraction blank 1", "EB0108.1" = "Extraction blank 2", 
-                                 "qPCRblank" = "qPRC blank"))
+  theme(axis.text.x = element_text(angle = 45, vjust =0.6),
+        axis.line.y.right = element_blank()) +
+  scale_color_discrete(labels = c("CT0884" = "Start-of-day 1", "CT0837" = "End-of-day 1", "CT0931" = "Start-of-day 2",
+                                  "CT0917" = "End-of-day 2", "EB0108" = "Extraction blank 1", "EB0108.1" = "Extraction blank 2", 
+                                 "qPCRBlank" = "qPRC blank")) +
+  scale_y_break(breaks = c(2000, 30000), scales = 0.35, expand = TRUE)
+  #scale_color_manual(values = pal)
+
